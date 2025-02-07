c94fe8d44b3383938b936afc9281df8c40f64af8
akif <su92-bssem-f22-118@superior.edu.pk>
2025-02-07 18:58:34 +0500

    kn

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 import { StudentService } from './student/student.service';
 import { StudentController } from './student/student.controller';
-
 import { HodModule } from './hod/hod.module';
 import { AttendenceModule } from './attendence/attendence.module';
 import { AttendenceService } from './attendence/attendence.service';
@@ -12,10 +10,26 @@ import { DepartmentModule } from './department/department.module';
 import { AdminModule } from './admin/admin.module';
 import { TeacherModule } from './teacher/teacher.module';
 import { StudentModule } from './student/student.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { student } from './student/student.entity';
+import { teacher } from './teacher/teacher.entity';
+import { Admin } from 'typeorm';
+import { Course } from './course/course.entity';
+import { Department } from './department/department.entity';
 @Module({
-  imports: [StudentModule, TeacherModule, AdminModule, DepartmentModule, AttendenceModule, HodModule],
-  controllers: [AppController, StudentController],
-  providers: [AppService, StudentService, CourseService, AttendenceService],
+  
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'school',
+      entities: [student,teacher,Admin,Course,Department],
+      synchronize: true,
+    }),StudentModule, TeacherModule, AdminModule, DepartmentModule, AttendenceModule, HodModule],
+  controllers: [StudentController],
+  providers: [ StudentService, CourseService, AttendenceService],
 })
 export class AppModule {}
---
+++ backend/src/department/department.entity.ts
@@ -0,0 +1,31 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToOne, OneToMany, JoinColumn, CreateDateColumn, UpdateDateColumn } from "typeorm";
+import { teacher } from "src/teacher/teacher.entity";
+import { Course } from "src/course/course.entity";
+import { student } from "src/student/student.entity";
+import { HOD } from "src/hod/hod.entity";
+
+@Entity()
+export class Department {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column({ type: "text", nullable: true })
+    description: string;
+
+    @OneToOne(() => teacher)
+    @JoinColumn()
+    teacher: teacher;  // Head of Department (One-to-One)
+    @OneToOne(() => HOD, (hod) => hod.department, { cascade: true })
+    hod: HOD;
+    @OneToMany(() => Course, (course) => course.department)
+    courses: Course[];  // A department can have multiple courses (One-to-Many)
+    @OneToOne(() => student, (student) => student.id)
+    student: student[];
+    @CreateDateColumn()
+    createdAt: Date;
+    @UpdateDateColumn()
+    updatedAt: Date;
+}
