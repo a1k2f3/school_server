80222c68238b2d9793a9bf3df43bcd5ac6061434
akif <su92-bssem-f22-118@superior.edu.pk>
2025-02-08 19:41:55 +0500

    di
    ihe

--- backend/src/student/student.module.ts
+++ backend/src/student/student.module.ts
@@ -1,26 +1,11 @@
-import { Module } from '@nestjs/common';
-import { StudentService } from './student.service';
-import { StudentController } from './student.controller';
-// import { TypeOrmModule } from '@nestjs/typeorm';
-import { student } from './student.entity';
-import { CourseModule } from 'src/course/course.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { JwtModule } from '@nestjs/jwt';
-// import { jwtConstants } from './constants';
-import { jwtConstants } from './constants';
+import { Module } from '@nestjs/common';
+import { student } from './student.entity';
+import { StudentService } from './student.service';
+
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([student]), // Ensure this line is present
-    CourseModule,
-    JwtModule.register({
-      global: true,
-      secret: jwtConstants.secret,
-      signOptions: { expiresIn: '30m' },
-    }),
-  ],
+  // imports: [TypeOrmModule.forFeature([student])],
   providers: [StudentService],
-  controllers: [StudentController],
-  exports: [StudentService], // Export StudentService if needed elsewhere
+  exports: [StudentService],
 })
 export class StudentModule {}
-
