630a0db5ed72c6270c1d193501e004e1a33827db
akif <su92-bssem-f22-118@superior.edu.pk>
2025-02-09 15:43:15 +0500

    jif
    ij

--- backend/src/student/student.entity.ts
+++ backend/src/student/student.entity.ts
@@ -6,10 +6,18 @@ import { Course } from '../course/course.entity';
 export class student {  // ✅ Ensure class name is capitalized as "Student"
   @PrimaryGeneratedColumn()
   id: number;
-
   @Column()
   name: string;
-
+   @Column({unique:true})
+   Email:string
+   @Column({unique:true})
+   contact:number
+   @Column()
+   date:Date
+   @Column()
+   password:string
+   @Column()
+   image:string
   @ManyToOne(() => Department, (department) => department.student)
   department: Department;
 
